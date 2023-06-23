832e96b53b124f78ff33cc4fb38e4b832f612d2a
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2023-06-22 21:49:05 -0300

    ci

--- tests/fucaoSimples.test.js
+++ tests/fucaoSimples.test.js
@@ -2,5 +2,6 @@ const { fucaoSimples } = require("../funcaoSimples");
 
 test("Teste fucaoSimples deve ser executado corretamente", () => {
   const resultado = fucaoSimples(5, 5);
+
   expect(resultado).toBe(10);
 });
